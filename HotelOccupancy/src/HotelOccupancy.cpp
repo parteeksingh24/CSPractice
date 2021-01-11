78b80122fb9f78237ff9df8baadb64dc9f5928d1
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-01-11 14:12:44 -0800

    Updated HotelOccupancy.cpp

--- HotelOccupancy/src/HotelOccupancy.cpp
+++ HotelOccupancy/src/HotelOccupancy.cpp
@@ -14,24 +14,24 @@ int main() {//main
 //Variables:
 	int floors 				= 0;	//Number of floors in hotel
 	int rooms 				= 0;	//Number of rooms (for each floor of hotel)
-	int occupied				= 0;	//Stores the # of occupied rooms (for each floor)
-	int totalRooms				= 0;	//Adds up all rooms from all floors
-	int totalOccupied 			= 0;	//How many total rooms are currently occupied
-	float percentOccupied 			= 0.0;	//Occupancy rate for hotel (how many rooms are filled)
-	const int MIN_FLOORS			= 1;	//Based on assignment, # of floors must be 1 and above
-	const int MIN_ROOMS			= 10;	//Each floor must have at least 10 rooms, based on assignment
+	int occupied			= 0;	//Stores the # of occupied rooms (for each floor)
+	int totalRooms			= 0;	//Adds up all rooms from all floors
+	int totalOccupied 		= 0;	//How many total rooms are currently occupied
+	float percentOccupied 	= 0.0;	//Occupancy rate for hotel (how many rooms are filled)
+	const int MIN_FLOORS	= 1;	//Based on assignment, # of floors must be 1 and above
+	const int MIN_ROOMS		= 10;	//Each floor must have at least 10 rooms, based on assignment
 
-	const int REMOVED_FLOOR			= 13;	/* Some hotels don't include a 13th floor, but this can
-							   be used to also shut down a floor for maintenance, etc.
-							*/
+	const int REMOVED_FLOOR	= 13;	/*	Some hotels don't include a 13th floor, but this can
+										be used to also "shut down" a floor for maintenance, etc.
+									*/
 
 //Find number of floors in hotel:
 	cout << "Please enter the number of floors: ";
-	cin  >> floors;
+	cin >> floors;
 	while(floors < MIN_FLOORS) {//while
 		cout << "Error! The number of floors cannot be less than " << MIN_FLOORS << endl;
 		cout << "Please enter the number of floors: ";
-		cin  >> floors;
+		cin >> floors;
 	}//while
 	cout << endl;
 
@@ -42,37 +42,37 @@ int main() {//main
 		}//if
 
 		cout << "How many rooms are on floor " << i << "?: ";
-		cin  >> rooms;
+		cin >> rooms;
 
 		while(rooms < MIN_ROOMS) {//while
 			cout << "Error! The number of rooms cannot be less than " << MIN_ROOMS << endl;
 			cout << "How many rooms are on floor " << i << "?: ";
-			cin  >> rooms;
+			cin >> rooms;
 		}//while
 
 		cout << "How many rooms are occupied?: ";
-		cin  >> occupied;
+		cin >> occupied;
 
 		while(occupied > rooms) {//while
 			cout << "Error! The number of occupied rooms can't be more than " << rooms << endl;
 			cout << "How many rooms are occupied?: ";
-			cin  >> occupied;
+			cin >> occupied;
 		}//while
 
 		cout << endl;
 
-	//Calculate total rooms in hotel, and total occupied from user input for each floor
-		totalRooms 	= totalRooms + rooms;
+	//Calculate total rooms in hotel, total occupied from user for each floor w/ "running totals"
+		totalRooms 		= totalRooms + rooms;
 		totalOccupied 	= totalOccupied + occupied;
-	}//FOR loop end
+	}//FOR loop END
 
 //Calculate the occupancy rate for hotel:
 	cout << endl;
 	percentOccupied = ( (static_cast<double>(totalOccupied))  / totalRooms) * 100;
 
 //Display all information and end program:
-	cout << "Number of rooms is "  << totalRooms << endl;
-	cout << "There are currently " << totalOccupied << " occupied rooms" << endl;
+	cout << "Our hotel has "  << totalRooms << " rooms!" 					<< endl;
+	cout << "There are currently " << totalOccupied << " occupied rooms" 	<< endl;
 
 	if((totalRooms - totalOccupied) != 0) {//if
 		cout << (totalRooms - totalOccupied) << " rooms are available at the moment" << endl;
