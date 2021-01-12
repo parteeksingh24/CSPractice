df01396f77be7ba3b328a1c5b3dd67d1339609c0
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-01-11 18:44:52 -0800

    Added user input, "countDown" function

--- Patterns/src/Patterns.cpp
+++ Patterns/src/Patterns.cpp
@@ -9,17 +9,31 @@
 #include <iostream>
 using namespace std;
 
-void countUp(void);
+//Function declarations:
+void countUp(int);
+void countDown(int);
 
+//Start of main:
 int main() {//main
 
-	countUp();
+//Variables:
+	int numRows = 0;
+
+//Determine sign of pattern:
+	cout << "Enter the number of rows you would like: ";
+	cin >> numRows;
+
+//Call the function to display plus signs (+) in ascending order:
+	cout << endl;
+	countUp(numRows);
 	cout << "Program ending, have a nice day!" << endl;
 	return 0;
 }//main
 
-void countUp(void) {//countUp
-	for(int level = 1; level <= 10; level++) {//outer
+//-----------------------------------------------------
+//FUNCTIONS:
+void countUp(int rows) {//countUp
+	for(int level = 1; level <= rows; level++) {//outer
 		for(int line = 1; line <= level; line++) {//inner
 			cout << "+ ";
 		}//INNER loop
@@ -29,3 +43,14 @@ void countUp(void) {//countUp
 	}//OUTER loop
 	cout << endl;
 }//countUp
+
+void countDown(int rows) {//countDown
+	for(int level = rows; level >= 1; --level) {//outer
+		for(int line = 1; line <= level; ++line) {//inner
+			cout << "+ ";
+		}//INNER loop
+
+		cout << "\n";
+	}//OUTER loop
+	cout << endl;
+}//countDown
