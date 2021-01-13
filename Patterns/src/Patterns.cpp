f67beab6526d02340332c02219d19a4941ec6a4e
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-01-12 23:06:36 -0800

    Added "pyramid" function

--- Patterns/src/Patterns.cpp
+++ Patterns/src/Patterns.cpp
@@ -12,20 +12,21 @@ using namespace std;
 //Function declarations:
 void countUp(int);
 void countDown(int);
+void pyramid(int);
 
 //Start of main:
 int main() {//main
 
 //Variables:
-	int numRows = 0;
+	int numRows = 20;
 
 //Determine sign of pattern:
 	cout << "Enter the number of rows you would like: ";
 	cin >> numRows;
 
-//Call the function to display plus signs (+) in ascending order:
+//Call the function to display a pyramid design:
 	cout << endl;
-	countUp(numRows);
+	pyramid(numRows);
 	cout << "Program ending, have a nice day!" << endl;
 	return 0;
 }//main
@@ -45,8 +46,8 @@ void countUp(int rows) {//countUp
 }//countUp
 
 void countDown(int rows) {//countDown
-	for(int level = rows; level >= 1; --level) {//outer
-		for(int line = 1; line <= level; ++line) {//inner
+	for(int level = rows; level >= 1; level--) {//outer
+		for(int line = 1; line <= level; line++) {//inner
 			cout << "+ ";
 		}//INNER loop
 
@@ -54,3 +55,17 @@ void countDown(int rows) {//countDown
 	}//OUTER loop
 	cout << endl;
 }//countDown
+
+void pyramid(int rows) {//pyramid
+	for(int i = 1; i <= rows; i++) {//outer
+		for(int space = 1; space <= rows - i; space++) {//inner (to create spaces)
+			cout << "  ";
+		}//INNER (space)
+
+		for(int star = 1; star <= (2*i) - 1; star++) {//inner for star symbol
+			cout << "* ";
+		}//for (star pattern)
+		cout << endl;
+	}//OUTER
+	cout << endl;
+}//pyramid
