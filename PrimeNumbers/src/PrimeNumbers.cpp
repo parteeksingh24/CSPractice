e98cc3df9b579b6e8164800568c1a028f2ebad0f
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-01-13 23:01:56 -0800

    Added functions to display primes within a range

--- PrimeNumbers/src/PrimeNumbers.cpp
+++ PrimeNumbers/src/PrimeNumbers.cpp
@@ -7,16 +7,25 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 bool isPrime(int);
+vector<int> findPrimes(int, int);
+void print(vector<int> const&);
 
 int main() {//main
 
-	cout << isPrime(5) << endl;		//1
-	cout << isPrime(4) << endl;		//0
+	cout << isPrime(5) << endl;		//1, true
+	cout << isPrime(4) << endl;		//0, false
 	cout << isPrime(22) << endl;	//0
 	cout << isPrime(991) << endl;	//1
+
+//Print primes from 1 to 100:
+	cout << endl;
+	cout << "Here are all the primes from 1 to 100: " << endl;
+	print(findPrimes(1, 100));
+	cout << endl;
 
 	cout << "Program ending, have a nice day!" << endl;
 	return 0;
@@ -41,3 +50,21 @@ bool isPrime(int num) {//checkPrime
 	}//else
 
 }//checkPrime
+
+vector<int> findPrimes(int start, int end) {//findPrimes (from starting # up to end #)
+	vector<int> list;
+
+	for(int i = start; i <= end; i++) {//for
+		if(isPrime(i) == true) {//if
+			list.push_back(i);
+		}//if
+	}//for
+
+	return list;
+}//displayPrimes
+
+void print(vector<int> const &primes) {//printPrimes
+	for(int i = 0; i < primes.size(); i++) {//for
+		cout << primes.at(i) << " ";
+	}//for
+}//printPrimes
