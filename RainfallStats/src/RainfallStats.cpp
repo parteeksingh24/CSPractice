fbf99726d0b6bc65a4e236e057b78e70cb5acc1a
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-01-14 22:25:27 -0800

    Initial commit

---
+++ RainfallStats/src/RainfallStats.cpp
@@ -0,0 +1,25 @@
+//============================================================================
+// Name        : RainfallStats.cpp
+// Author      : Parteek Singh
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+/* Prompt:
+ 	Write a program that lets the user enter the total rainfall for each of 12 months
+ 	 into an array of doubles. The program should calculate and display the total rainfall
+ 	 for the year, the average monthly rainfall, & the months with the highest & lowest amounts.
+
+ 	Input Validation: Do not accept negative numbers for monthly rainfall figures.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {//main
+
+
+	cout << "Program ending, have a nice day!" << endl;
+	return 0;
+}//main
