c81c718602ab599d16a6d545776d64e39b736c7b
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-01-16 20:36:15 -0800

    Added functions to expand, shift arrays (in main.cpp)

--- ReverseArray/src/main.cpp
+++ ReverseArray/src/main.cpp
@@ -10,11 +10,13 @@
 using namespace std;
 
 int* reverse(int[], int);
+int* expand(int[], int);
+int* shift(int[], int);
 
 int main() {//main
 //Variables used:
 	const int SIZE = 5;
-	int* reverseArr = nullptr;
+	int* arrPtr = nullptr;
 
 //Create array of integers with initialized values and print this array:
 	int numbers[SIZE] = {5, 10, 25, 100, 2};
@@ -26,13 +28,33 @@ int main() {//main
 //Call the reverse functions with this array and print:
 	cout << endl << endl;
 	cout << "Now in reverse: " << endl;
-	reverseArr = reverse(numbers, SIZE);
+	arrPtr = reverse(numbers, SIZE);
 	for(int i = 0; i < SIZE; i++) {//for
-		cout << "\t" << *(reverseArr + i) << " ";
+		cout << "\t" << *(arrPtr + i) << " ";
+		//cout << arrPtr[i] << endl;
 	}//for
 
+//Expand the array and print:
+	cout << endl << endl;
+	cout << "Doubling the array: " << endl;
+	arrPtr = expand(arrPtr, SIZE);
+	for(int i = 0; i < (2 * SIZE); i++) {//for
+		cout << "\t" << *(arrPtr + i) << " ";
+	}//for
+
+//Shift the array and print:
+	cout << endl << endl;
+	cout << "Now after we shift the array: " << endl;
+	arrPtr = shift(arrPtr, SIZE);
+	for(int i = 0; i < (2*SIZE + 1); i++) {//for
+		cout << "\t" << *(arrPtr + i) << " ";
+	}//for
+
+//Free up dynamically allocated memory:
+	delete arrPtr;
+
 //End the program:
-	cout << endl;
+	cout << endl << endl;
 	cout << "Program ending, have a nice day!" << endl;
 	return 0;
 }//main
@@ -51,3 +73,30 @@ int* reverse(int arr[], int size) {//reverseArray
 	}//for
 	return arrayCopy;
 }//reverseArray
+
+int* expand(int arr[], int size) {//expandArray
+	int newSize = 2 * size;
+	int* copy = new int[newSize];
+
+	for(int i = 0; i < size; i++) {//for
+		copy[i] = arr[i];
+	}//for
+
+	for(int k = size; k < newSize; k++) {//for
+		copy[k] = 0;
+	}//for
+
+	return copy;
+}//expandArray
+
+int* shift(int arr[], int size) {//shiftArray
+	int newSize = size + 1;
+	int* shiftPtr = new int[newSize];
+	shiftPtr[0] = 0;
+
+	for(int i = 1; i <= newSize; i++) {//for
+		shiftPtr[i] = arr[i - 1];
+	}//for
+
+	return shiftPtr;
+}//shiftArray
