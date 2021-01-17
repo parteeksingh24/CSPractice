83f00c9ea7d5c1ddb6401048d1784389d82e5722
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-01-17 10:17:49 -0800

    Updated input validation

--- ChipsAndSalsa/src/ChipsAndSalsa.cpp
+++ ChipsAndSalsa/src/ChipsAndSalsa.cpp
@@ -26,7 +26,7 @@ int main() {//main
 		cout << salsa[i] << ": ";
 		cin  >> jars[i];		//Read in the input into the array for sales numbers
 
-		while(amount < 0) {//while
+		while(jars[i] < 0) {//while
 			cout << "ERROR: Please enter a positive number only." 					<< endl;
 			cout << salsa[i] << ": ";
 			cin  >> jars[i];	//Read in the input into the array for sales numbers
@@ -36,16 +36,16 @@ int main() {//main
 //Print information:
 	cout << endl << "Salsa sales report" << endl;
 	cout << 		"==================" << endl;
-	cout << "Total sales: " 	 				<< getTotal(jars) 			<< endl << endl;
+	cout << "Total sales: " 	 					<< getTotal(jars) 		<< endl << endl;
 
 	for(int i = 0; i < SIZE; i++) {//for
 		cout << "# of " << salsa[i] << " sold is " 	<< jars[i] 						<< endl;
 	}//for
 
-	cout << endl << salsa[largest(jars)] << " had the most sales, with "
+	cout << endl << salsa[largest(jars)] 	<< " had the most sales, with "
 				 << jars[largest(jars)] 	<< " sold" 								<< endl;
-	cout << salsa[smallest(jars)] << " sold the fewest jars, only selling "
-				 << jars[smallest(jars)] << " jars" 								<< endl;
+	cout << salsa[smallest(jars)] 			<< " sold the fewest jars, only selling "
+				 << jars[smallest(jars)] 	<< " jar(s)" 								<< endl;
 
 //End program:
 	cout << endl << "Program ending, have a nice day!" 								<< endl;
