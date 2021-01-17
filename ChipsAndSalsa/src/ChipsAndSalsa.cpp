98fd762dd2e2a535fcae7a017be54aea49a17453
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-01-17 10:11:22 -0800

    Updated ChipsAndSalsa.cpp

--- ChipsAndSalsa/src/ChipsAndSalsa.cpp
+++ ChipsAndSalsa/src/ChipsAndSalsa.cpp
@@ -21,29 +21,34 @@ int main() {//main
 	int amount = 0;	//User input for amount of each salsa type sold
 
 //Find the number of jars sold for each salsa type:
-	cout << "Please enter the amount of jars sold for each salsa below. " << endl;
+	cout << "Please enter the amount of jars sold for each salsa below. " 			<< endl;
 	for(int i = 0; i < SIZE; i++) {//for
 		cout << salsa[i] << ": ";
-		cin  >> amount;
+		cin  >> jars[i];		//Read in the input into the array for sales numbers
 
 		while(amount < 0) {//while
-			cout << "ERROR: Please enter a positive number only." << endl;
-			cout << "# of " << salsa[i] << "sold: ";
-			cin  >> amount;
+			cout << "ERROR: Please enter a positive number only." 					<< endl;
+			cout << salsa[i] << ": ";
+			cin  >> jars[i];	//Read in the input into the array for sales numbers
 		}//while
-
-		jars[i] = amount;
 	}//for
 
 //Print information:
 	cout << endl << "Salsa sales report" << endl;
 	cout << 		"==================" << endl;
+	cout << "Total sales: " 	 				<< getTotal(jars) 			<< endl << endl;
+
 	for(int i = 0; i < SIZE; i++) {//for
-		cout << "# of " << salsa[i] << " sold is " 	<< jars[i] 			<< endl;
-		cout << "Total sales: " 	 				<< getTotal(jars) 	<< endl;
+		cout << "# of " << salsa[i] << " sold is " 	<< jars[i] 						<< endl;
 	}//for
 
-	cout << "Program ending, have a nice day!" << endl;
+	cout << endl << salsa[largest(jars)] << " had the most sales, with "
+				 << jars[largest(jars)] 	<< " sold" 								<< endl;
+	cout << salsa[smallest(jars)] << " sold the fewest jars, only selling "
+				 << jars[smallest(jars)] << " jars" 								<< endl;
+
+//End program:
+	cout << endl << "Program ending, have a nice day!" 								<< endl;
 	return 0;
 }//main
 
@@ -59,5 +64,21 @@ int getTotal(int arr[]) {//totalSales
 
 int largest(int arr[]) {//mostSold
 	int largest = 0;	//index of the element with highest value
-
+	for(int i = 1; i < 5; i++) {//for
+		if(arr[largest] <= arr[i]) {//if
+			largest = i;
+		}//if
+	}//for
+	return largest;
 }//mostSold
+
+int smallest(int arr[]) {//leastSold
+	int smallest = 0;	//index of the element with smallest value
+	for(int i = 1; i < 5; i++) {//for
+		if(arr[smallest] >= arr[i]) {//if
+			smallest = i;
+		}//if
+	}//for
+	return smallest;
+}//leastSold
+
