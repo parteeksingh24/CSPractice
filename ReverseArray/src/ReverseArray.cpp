939dc9728bbd50ab0916e64c93dda045050f4eea
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-01-16 19:55:40 -0800

    Fixed bug in reversing function (in ReverseArray.cpp)

--- ReverseArray/src/ReverseArray.cpp
+++ ReverseArray/src/ReverseArray.cpp
@@ -12,7 +12,27 @@ using namespace std;
 int* reverse(int[], int);
 
 int main() {//main
+//Variables used:
+	const int SIZE = 5;
+	int* reverseArr = nullptr;
 
+//Create array of integers with initialized values and print this array:
+	int numbers[SIZE] = {5, 10, 25, 100, 2};
+	cout << "Here is the original array: " << endl;
+	for(int i = 0; i < SIZE; i++) {//for
+		cout << "\t" << numbers[i] << " ";
+	}//for
+
+//Call the reverse functions with this array and print:
+	cout << endl << endl;
+	cout << "Now in reverse: " << endl;
+	reverseArr = reverse(numbers, SIZE);
+	for(int i = 0; i < SIZE; i++) {//for
+		cout << "\t" << *(reverseArr + i) << " ";
+	}//for
+
+//End the program:
+	cout << endl;
 	cout << "Program ending, have a nice day!" << endl;
 	return 0;
 }//main
@@ -24,9 +44,10 @@ int* reverse(int arr[], int size) {//reverseArray
 	int counter = 0;
 
 	for(int i = (size - 1); i >= 0; i--) {//for
-		while(counter <= size) {//while
+		if(counter <= size) {//if
 			arrayCopy[i] = arr[counter];
 			counter++;
-		}//while
+		}//if
 	}//for
+	return arrayCopy;
 }//reverseArray
