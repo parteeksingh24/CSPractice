190672b34ea07bd6b7f36fdf34d9a01c57450176
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-01-18 15:37:20 -0800

    Updated main.cpp

--- FunWithStrings/src/main.cpp
+++ FunWithStrings/src/main.cpp
@@ -7,16 +7,25 @@
 //============================================================================
 
 #include <iostream>
+//#include <cctype>
 using namespace std;
 
-int length (char []);
-void reverse(char*);
+int 	length (char []);
+void 	reverse(char*);
+int 	wordCount(const char*);
+int 	wordCount(string);		//Overloaded version of the "wordCount" function
 
 int main(void) {//main
 	char str[] = "Hello";
 
 	cout << length(str) << endl;
 	reverse(str);
+
+	char message[] = "Four score and seven years ago";
+	cout << wordCount(message) << endl;						//Will print: 6
+
+	string text = "C++ is very fun!";
+	cout << wordCount(text)		<< endl;				//Will call the overloaded string function, print 4
 
 
 	cout << "Program ending, have a nice day!" << endl;
@@ -38,6 +47,7 @@ int length(char word[]) {//stringLength
 
 void reverse(char* str) {//reverse
 	int position = length(str) - 1;	//Start at the last character of c-string
+										//Calls the "length" function found in main
 
 	while(position >= 0) {//while
 		cout << *(str + position) << " ";
@@ -45,3 +55,41 @@ void reverse(char* str) {//reverse
 	}//while
 	cout << endl;
 }///reverse
+
+int wordCount(const char* word) {//wordCount (c-string)
+	int counter = 0;
+
+	while(*word != '\0') {//while
+		if(isspace(*word) == true) {//if
+			counter++;
+		}//if
+
+		word++;				//Increment the char pointer through the c-string (array)
+	}//while
+
+	return counter + 1;		//The # of words will be one more than the # of spaces
+}//wordCount (c-string)
+
+int wordCount(string word) {//wordCount (string)
+//We can convert our string to a c-string and use the wordCount function above:
+	const char* ptr = word.c_str();	/* NOTE:
+										the c_str() function returns a constant char,
+										so we must make sure the parameter of our
+										original "wordCount" function is also a const char.
+									*/
+
+	return wordCount(ptr);
+
+
+/* For the independent function, see below:
+	int counter = 0;
+
+	for(int i = 0; i < word.size(); i++) {//for
+		if(isspace(word[i]) == true) {//if
+			counter++;
+		}//if
+	}//for
+
+	return counter;
+*/
+}//wordCount (string)
