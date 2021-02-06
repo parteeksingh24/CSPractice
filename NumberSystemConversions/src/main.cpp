e80e9fe2a0c2f8832cc60a8020713f9005eecb25
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-02-06 13:52:56 -0800

    Updated main.cpp
    Added exceptions in "binaryToDecimal" function for input that is not
    binary, and for input that is larger than 8 digits.
    
    Added exceptins in "hexToDecimal" for inputs that is not a hexadecimal
    value (0-9, a-f), and for input that is larger than 4 digits.
    
    Updated the switch statement cases for both functions to include
    try/catch blocks that handle the given exceptions.

--- NumberSystemConversions/src/main.cpp
+++ NumberSystemConversions/src/main.cpp
@@ -86,8 +86,8 @@ int main() {//main
 				getline(cin, decimalNum);
 
 				string binaryValue = decimalToBinary(decimalNum);
-				cout << "The number " << decimalNum 	<< " in binary is: "
-						<< binaryValue		<< endl;
+				cout << "The number " 	<< decimalNum 	<< " in binary is: "
+										<< binaryValue	<< endl;
 			}//try
 			catch(invalid_argument &error1) {//catch
 				cout << "Error: Please enter a decimal value!\n"
@@ -104,30 +104,17 @@ int main() {//main
 			break;
 		case 2:
 			//Call binaryToDecimal function
-			cout << "Enter a binary number up to 8 bits long: ";
-			getline(cin, userInput);
+			try {//try
+				cout << "Enter a binary number up to 8 bits long: ";
+				getline(cin, userInput);
 
-			if(checkBinary(userInput) == false) {//if (the given input is not binary)
-				if(userInput.length() > 8) {//if	(the string entered is longer than 8 characters)
-					cout << "Error: Please enter a binary number\n"
-							"\t(only 1s and 0s) up to 8 digits long." 					<< endl;
-				}//if
-				else {//else
-					//when the given value is less than 8 characters, the only error is not having all binary values
-					cout << "Error: Please enter a binary number (only 1s and 0s)."		<< endl;
-				}//else
-
-				break;
-			}//if
-			else if(userInput.length() > 8) {//else if	(the input is in binary form, but longer than 8 bits)
-				cout << "Error: The maximum length is 8 digits, please try again."		<< endl;
-
-				break;
-			}//else if
-			else {//else	(the input is both binary AND less than or equal to 8 bits long, so we convert and display result)
+				int decValue = binaryToDecimal(userInput);
 				cout << "The number "	<< userInput 	<< " in decimal is: "
-						<< binaryToDecimal(userInput)	<< endl;
-			}//else
+										<< decValue		<< endl;
+			}//try
+			catch(string &badInput) {//catch
+				cout << badInput << endl;
+			}//catch
 
 			break;
 		case 3:
@@ -137,7 +124,7 @@ int main() {//main
 				getline(cin, decimalNum);
 
 				string hexValue = decimalToHex(decimalNum);
-				cout << "The number " 	<< decimalNum 	<< " in binary is: "
+				cout << "The number " 	<< decimalNum 	<< " in hexadecimal is: "
 										<< hexValue		<< endl;
 			}//try
 			catch(invalid_argument &error1) {//catch
@@ -155,31 +142,17 @@ int main() {//main
 			break;
 		case 4:
 			//Call hexToDecimal function
-			cout << "Enter a hexadecimal number, up to 4 digits long: ";
-			getline(cin, userInput);
+			try {//try
+				cout << "Enter a hexadecimal number, up to 4 digits long: ";
+				getline(cin, userInput);
 
-			if(checkHexadecimal(userInput) == false) {//if (the given input is not a string of 0-9, a-f)
-				if(userInput.length() > 4) {//if
-					cout << "Error: Please enter a hexadecimal number up to 4 digits long.\n"
-							"\tThe options are the numbers 0-9, and the letters a-f." << endl;
-				}//if
-				else {//else
-					cout << "Error: Please enter a hexadecimal number. \n"
-							"\tThe options are the numbers 0-9, and the letters a-f." << endl;
-				}//else
-
-				break;
-			}//if
-			else if(userInput.length() > 4) {//else if
-				cout << "Error: The maximum length for the number is 4 digits.\n"
-						"\tPlease try again." 		<< endl;
-
-				break;
-			}//else if
-			else {//else
-				cout << "The number " << userInput 	<< " in decimal is: "
-						<< hexToDecimal(userInput) 	<< endl;
-			}//else
+				int decimalVal = hexToDecimal(userInput);
+				cout << "The number " 	<< userInput 	<< " in decimal is: "
+										<< decimalVal 	<< endl;
+			}//try
+			catch(string &invalidInput) {//catch
+				cout << invalidInput << endl;
+			}//catch
 
 			break;
 		case EXIT_CODE:
@@ -190,6 +163,7 @@ int main() {//main
 			//If none of the options are chosen, an error message will display
 			cout << "Error: Please select an option from the menu below. \n"
 			<< "\tThe valid choices are the numbers 1, 2, 3, 4, and 9." 			<< endl;
+
 		}//switch
 	}//while
 
@@ -301,18 +275,32 @@ string	decimalToBinary(string str) {//decToBinary
 }//decToBinary
 
 int	binaryToDecimal(string inputStr) {//binaryToDecimal
-	string binaryNum 	= reverse(inputStr);	//Start from least significant bit (LSB)
-	int decimalNum		= 0;					//The converted number in decimal form
+	string 	binaryNum 	= "";		//Will hold the binary input, granted it is a valid binary string
+	int 	decimalNum	= 0;		//The converted number in decimal form to be returned
+	string	badInput	= "";		//Holds any exception thrown, if binary string given is invalid
 
-	for(int i = 0; i < binaryNum.size(); i++) {//for
-		int temp = binaryNum[i] - '0';	//Converts the ASCII value of the char to its numerical value
-		//To convert the ASCII value to an integer value, we can subtract the value of the zero character, '0'
-		//Recall that the ASCII character '0' is 48 in decimal, and a digit like '9' is 57, so when we subtract
-		//by the '0', we are essentially subtracting the difference in decimal form, to get 9 in this case
+	if(checkBinary(inputStr) == false) {//if
+		badInput	= "Error: Please enter a binary number (only include 1s and 0s).";
+		throw badInput;
+	}//if
+	else if(inputStr.length() > 8) {//else if
+		badInput	= "Error: The maximum length is 8 digits, please try again.";
+		throw badInput;
+	}//else if
+	else {//else
+		binaryNum	= reverse(inputStr);	//Start from least significant bit (LSB)
+		int temp	= 0;					//Will hold the numerical value of the characters in string, based on ASCII table
 
-		//(it figures out the distance from '0')
-		decimalNum += ( (pow(2, i)) * temp );	//Multiply by the corresponding power of 2
-	}//for
+		for(int i = 0; i < binaryNum.size(); i++) {//for
+			temp = binaryNum[i] - '0';	//Converts the ASCII value of the char to its numerical value
+			//To convert the ASCII value to an integer value, we can subtract the value of the zero character, '0'
+			//Recall that the ASCII character '0' is 48 in decimal, and a digit like '9' is 57, so when we subtract
+			//by the '0', we are essentially subtracting the difference in decimal form, to get 9 in this case
+
+			//(it figures out the distance from '0')
+			decimalNum += ( (pow(2, i)) * temp );	//Multiply by the corresponding power of 2
+		}//for
+	}//else
 
 	return decimalNum;
 }//binaryToDecimal
@@ -352,21 +340,36 @@ string	decimalToHex(string str) {//toHexadecimal
 }//toHexadecimal
 
 int	hexToDecimal(string hexValue) {//hexToDecimal
-	string reverseValue = reverse(hexValue);	//Allows our for loop to increment up, starting from least significant digit
-	int temp			= 0;	//Stores the converted hex values as their decimal equivalents
-	int decimalNum 		= 0;	//Holds the running total for the decimal value
+	string 	reverseValue 	= "";
+	int 	decimalNum		= 0; 	//Holds the running total for the decimal value
+	string	invalidInput	= "";
 
-	for(int i 	= 0; i < reverseValue.size(); i++) {//for
-		if( reverseValue[i] >= '0' && reverseValue[i] <= '9') {//if
-			temp	= reverseValue[i] - 48;
-		}//if
-		else if(reverseValue[i] >= 'a' && reverseValue[i] <= 'f') {//else if
-			temp	= reverseValue[i] - 87;
-		}//else if
-		else {}
+	if(checkHexadecimal(hexValue) == false) {//if
+		invalidInput = "Error: Please enter a hexadecimal number. \n"
+						"\tThe options are the numbers 0-9, and the letters a-f.";
+		throw invalidInput;
+	}//if
+	else if(hexValue.length() > 4) {//else if
+		invalidInput = "Error: The maximum length for the number is 4 digits.\n"
+						"\tPlease try again.";
+		throw invalidInput;
+	}//else if
+	else {//else
+		reverseValue = reverse(hexValue); //Allows our for loop to increment up, starting from least significant digit
+		int temp = 0;	//Stores the converted hex values as their decimal equivalents
 
-		decimalNum	= decimalNum + ( (pow(16, i)) * temp );
-	}//for
+		for(int i 	= 0; i < reverseValue.size(); i++) {//for
+			if( reverseValue[i] >= '0' && reverseValue[i] <= '9') {//if
+				temp	= reverseValue[i] - 48;
+			}//if
+			else if(reverseValue[i] >= 'a' && reverseValue[i] <= 'f') {//else if
+				temp	= reverseValue[i] - 87;
+			}//else if
+			else {}
+
+			decimalNum	= decimalNum + ( (pow(16, i)) * temp );
+		}//for
+	}//else
 
 	return decimalNum;
 }//hexToDecimal
