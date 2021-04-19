e6045dd1faa9f49920dd2b1ee68840dfb4972cc9
parteeksingh24 <56564155+parteeksingh24@users.noreply.github.com>
2021-04-19 16:56:59 -0700

    Initial commit

---
+++ CharacterSwap/src/CharacterSwap.cpp
@@ -0,0 +1,41 @@
+//============================================================================
+// Name        		: FindingMedian.cpp
+// Author			: Parteek Singh
+// Course			: CS Review
+// Last Modified   	: 04/19/2021
+// Description 		: For a given string, this function will swap all
+//						instances of a character with another, and vice versa.
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+string doubleSwap(string, char, char);	//Function prototype
+
+int main() {//main
+
+	cout <<	doubleSwap("aabbccc", 'a', 'b')		<< endl;	//bbaaccc
+	cout << doubleSwap("dzefadbbaf", 'a', 'f')	<< endl;	//dzeafdbbfa
+
+	cout << doubleSwap("1244526", '2', '8')		<< endl;	//1844586
+
+	cout << endl;
+	cout << "Program ending, have a nice day!" 	<< endl;
+	return 0;
+}//main
+
+//----------
+//Functions:
+//----------
+string doubleSwap(string inputStr, char c1, char c2) {//doubleSwap
+	for(int i = 0; i < inputStr.size(); i++) {//for
+		if(inputStr[i] == c1) {//if
+			inputStr[i] = c2;
+		}//if
+		else if(inputStr[i] == c2) {//if
+			inputStr[i] = c1;
+		}//if
+	}//for
+
+	return inputStr;
+}//doubleSwap
